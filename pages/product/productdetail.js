58eab5fd3693c450c8a62cf4bc000aa56d7abbc1
unknown <unknown@example.com>
2023-02-20 20:13:18 +0800

    20230220

--- pages/product/productdetail.js
+++ pages/product/productdetail.js
@@ -1,10 +1,26 @@
 const app = getApp()
-
 Page({
   /**
    * 页面的初始数据
    */
   data: {
+    newuserinfo: {
+      UserName: "",
+      nickName: "",
+      avatarUrl: "",
+      Region: ["广东省", "深圳市", "南山区"],
+      UserPhone: "",
+      CompanyName: "",
+      CompanyId: "",
+      BusinessScope: "",
+      CompanyScale: "",
+    },
+    newusertradeinfo: {
+      Balance: 0,
+      DiscountLevel: "DL4",
+      PromoterLevel: "normal",
+      UserType: "client"
+    },
     //通过对页面内容分区域设置隐藏，达到分栏显示效果,hidden要反着写，显示的值为false,不显示的值为true
     DetailHidden: false,
     QAHidden: true,
@@ -19,6 +35,7 @@ Page({
     answer: "",
     sublock: false,
     replylock: false,
+    tempimage:[],
     // 产品轮播参数
     swiperData: [],
     swiperHeight: "", // swiper的高度
@@ -246,6 +263,190 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
+  _productcheck() {
+    console.log("productcheck执行了")
+    wx.cloud.callFunction({
+      name: "NormalQuery",
+      data: {
+        collectionName: "PRODUCT",
+        command: "or",
+        where: [{
+          Status: "在售"
+        }]
+      },
+      success: res => {
+        app.globalData.Gproduct = res.result.data
+        wx.setStorageSync('LProductList', res.result.data)
+      }
+    })
+  },
+  _setting() {
+    //获取小程序全局设置
+    let that = this
+    const db = wx.cloud.database()
+    db.collection('setting')
+      .where({
+        currentstatus: "effect"
+      })
+      .get({
+        success: res => {
+          app.globalData.Gpointsmagnification = res.data[0].pointsmagnification;
+          app.globalData.Gsortarray = res.data[0].SortArray;
+          console.log("成功获取设置参数", res);
+          //异步获取图片生成轮播图地址
+          for (let i = 0; i < res.data[0].swiper.length; i++) {
+            wx.getImageInfo({
+              //把图片地址转换为本地地址
+              src: res.data[0].swiper[i],
+              success(res) {
+                that.data.tempimage.push(res.path)
+                app.globalData.Gimagearray = that.data.tempimage
+              }
+            })
+          }
+        }
+      })
+  },
+  _usercheck() {
+    console.log("usercheck执行中")
+    const db = wx.cloud.database()
+    db.collection('USER').where({
+      _openid: app.globalData.Gopenid,
+    }).get({
+      success: res => {
+        console.log("当前用户信息", res);
+        // 判断是否新用户并提交数据库起始
+        if (res.data.length == 0) {
+          this._newuser()
+        } else {
+          // 老用户如果云数据库中有本人信息，则把用户数据存入本地全局变量，以供后续使用
+          app.globalData.Guserdata = res.data[0]
+          app.globalData.Guserinfo = res.data[0].UserInfo
+          app.globalData.Gtradeinfo = res.data[0].TradeInfo
+
+          // 查询结果赋值给数组参数
+          //   this.setData({
+          //   user: res.data[0].UserInfo,
+          //  inviterid: res.data[0].InviterInfo.OpenId,
+          //  })
+          this._olduser()
+        }
+      }
+    })
+  },
+  _newuser() {
+    console.log("新用户操作执行了")
+    // 如果是新用户，检查是否有传递过来的推荐人id
+    this.setData({
+      inviterid: this.data.tempinviterid,
+    })
+
+    app.globalData.Ginviterid = this.data.tempinviterid
+    app.globalData.Guserinfo = this.data.newuserinfo
+    app.globalData.Gtradeinfo = this.data.newusertradeinfo
+    console.log("Ginviterid", app.globalData.Ginviterid)
+    console.log("Guserinfo", app.globalData.Guserinfo)
+    // 在USER数据库中新增用户信息
+    const db = wx.cloud.database()
+    db.collection("USER").add({
+      data: {
+        SysAddDate: new Date().getTime(),
+        AddDate: new Date().toLocaleString(),
+        Params: this.data.params,
+        UserInfo: this.data.newuserinfo,
+        SystemInfo: app.globalData.Gsysteminfo,
+        TradeInfo: this.data.newusertradeinfo,
+        Remark: this.data.remark,
+      },
+      success: res => {
+        console.log("新增用户数据执行成功")
+        // 查询推荐人信息
+        this._invitercheck()
+      },
+    })
+
+  },
+  _olduser() {
+    var that = this
+    console.log("未更新折扣级别", app.globalData.Gtradeinfo)
+    console.log("执行老用户价格等级查询")
+    // 老用户确认价格等级，这一步放在index操作是便于直接跳转到其他页面
+    const db = wx.cloud.database()
+    const _ = db.command
+    db.collection('DISCOUNTORDER').where({
+      _openid: app.globalData.Gopenid,
+      PaymentStatus: "checked",
+      OrderStatus: "checked",
+      Available: true,
+    }).orderBy('OrderId', 'desc').get({
+      success: res => {
+        console.log("已购买的价格折扣卡", res)
+        if (res.data.length != 0) {
+          var tempfliter = []
+          for (var i = 0; i < res.data.length; i++) {
+            if (new Date(res.data[i].DLStartDate).getTime() <= new Date().getTime() && new Date(res.data[i].DLEndDate).getTime() >= new Date().getTime()) {
+              tempfliter.push(res.data[i]);
+            }
+          }
+          if (tempfliter.length != 0 && tempfliter.length != undefined) {
+            console.log(tempfliter)
+            console.log(tempfliter[0].DiscountLevel)
+            // 更新对象型全局变量个别属性的方法
+            app.globalData.Gtradeinfo.DiscountLevel = tempfliter[0].DiscountLevel
+            app.globalData.Gtradeinfo.DiscountType = tempfliter[0].DiscountType
+          } else {
+            //卡券已过期            
+            app.globalData.Gtradeinfo.DiscountLevel = "DL4"
+          }
+        } else {
+          //没有卡券
+          app.globalData.Gtradeinfo.DiscountLevel = "DL4"
+        }
+        console.log("已更新折扣级别", app.globalData.Gtradeinfo)
+        // 查询推荐人信息
+        this._invitercheck()
+      }
+    })
+
+  },
+  _invitercheck() {
+    console.log("invitercheck执行了")
+    // 查询推荐人信息
+    const db = wx.cloud.database()
+    db.collection('USER').where({
+      _openid: app.globalData.Ginviterid
+    }).get({
+      success: res => {
+        app.globalData.Ginviter = res.data[0].UserInfo
+        this.setData({
+          indirectinviterid: res.data[0].InviterInfo.OpenId, //间接推荐人的id
+        })
+        // 把需要的推荐人信息构建成对象数组赋值给全局变量
+        var obj = new Object();
+        obj = {
+          "OpenId": res.data[0]._openid, //直接推荐人的id
+          "Name": res.data[0].UserInfo.UserName,
+          "Company": res.data[0].UserInfo.CompanyName,
+          "Phone": res.data[0].UserInfo.UserPhone,
+          "InviterOpenId": res.data[0].InviterInfo.OpenId, //间接推荐人的id
+          "PromoterLevel": res.data[0].PromoterLevel,
+          "DiscountLevel": res.data[0].DiscountLevel,
+          "Balance": res.data[0].Balance,
+        }
+        app.globalData.Ginviter = obj
+        db.collection('USER').where({
+          _openid: app.globalData.openid
+        }).update({
+          data: {
+            InviterInfo: obj
+          }
+        })
+
+        console.log(app.globalData.Ginviter)
+
+      },
+    })
+  },
   onLoad: function (options) {
     var that = this;
     console.log("页面接收参数", options)
@@ -253,6 +454,22 @@ Page({
     this.setData({
       pageParam: options,
     })
+    if (app.globalData.Guserinfo == undefined) {
+      // 通过云函数获取用户本人的小程序ID
+      wx.cloud.callFunction({
+        name: 'login',
+        data: {},
+        success: res => {
+          console.log("login成功:", res.result)
+          app.globalData.Gopenid = res.result.openid
+
+          // 查询小程序数据库是否有当前用户信息
+          this._usercheck()
+          this._setting()
+          this._productcheck()
+        }
+      })
+    }
     //判断全局变量是否有值，注意写法格式没有引号
     if (app.globalData.Gproduct == undefined) {
       //如果通过分享链接进入没有产品数据，则查询产品数据
@@ -267,10 +484,10 @@ Page({
         },
         success: res => {
           app.globalData.Gproduct = res.result.data
+          console.log(app.globalData.Gproduct)
         }
       })
     } else {
-
       // 筛选指定记录
       var fliter = [];
       // var _this = this
