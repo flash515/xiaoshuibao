9c441fc3dc3724e40f6cb8b1018a990a1bcdf18b
unknown <unknown@example.com>
2023-03-21 21:04:30 +0800

    20230321

--- pages/promote/pointspacket.js
+++ pages/promote/pointspacket.js
@@ -8,6 +8,12 @@ Page({
   data: {
     tempinviterid: "",
     params: "",
+    userphone: "",
+    useroldphone: "",
+    loginshow: true,
+    time: "获取验证码",
+    s_phonecode: "",
+    u_phonecode: "",
     remark: "积分红包",
     transferpacketid: "",
     transferpoints: 0,
@@ -17,6 +23,178 @@ Page({
     remainpacket: 0,
     temppoints: 0,
     temppacket: 0
+  },
+  bvUserPhone(e) {
+    this.setData({
+      userphone: e.detail.value
+    })
+  },
+  _SendCodeBtn() {
+
+    var that = this;
+    var currentTime = that.data.currentTime
+    interval = setInterval(function () {
+      currentTime--;
+      that.setData({
+        time: currentTime + '秒'
+      })
+      if (currentTime <= 0) {
+        clearInterval(interval)
+        that.setData({
+          time: '重新发送',
+          currentTime: 60,
+          disabled: false
+        })
+      }
+    }, 1000)
+
+  },
+  bvSendCode() {
+    if (this.data.userphone == "" || this.data.userphone == undefined) {
+      wx.showToast({
+        title: '请输入手机号码',
+        icon: 'error',
+        duration: 2000
+      })
+    } else {
+      let _this = this;
+
+      this.setData({
+        disabled: true
+      })
+      wx.cloud.callFunction({
+        name: 'sendmessage',
+        data: {
+          templateId: "985130",
+          nocode: false,
+          mobile: _this.data.userphone,
+          nationcode: '86'
+        },
+        success: res => {
+          let code = res.result.res.body.params[0];
+          let result = res.errMsg;
+          if (result == "cloud.callFunction:ok") {
+            _this.setData({
+              result: "发送成功",
+              s_phonecode: code
+            })
+            this._SendCodeBtn()
+          } else {
+            _this.setData({
+              result: "发送失败"
+            })
+          }
+        },
+        fail: err => {
+          console.error('[云函数] [sendsms] 调用失败', err)
+        }
+      })
+    }
+  },
+  bvPhoneCode(e) {
+    this.setData({
+      u_phonecode: e.detail.value
+    })
+  },
+  bvLogin(e) {
+
+    if (this.data.s_phonecode == this.data.u_phonecode && this.data.u_phonecode != "") {
+      console.log('手机验证码正确')
+      const db = wx.cloud.database()
+      db.collection('USER').where({
+        UserId: this.data.openid
+      }).update({
+        data: {
+          ["UserInfo.UserPhone"]: this.data.userphone,
+          ["UserInfo.UpdateDate"]: new Date().toLocaleString('chinese', {
+            hour12: false
+          })
+        },
+        success(res) {
+          wx.showToast({
+            title: '更新信息成功',
+            icon: 'success',
+            duration: 2000 //持续的时间
+          })
+        },
+        fail(res) {
+          wx.showToast({
+            title: '更新信息失败',
+            icon: 'error',
+            duration: 2000 //持续的时间
+          })
+        }
+      })
+      // 根据用户是否已验证手机号，提供首次验证积分
+      if (this.data.useroldphone == "") {
+        // 成为会员时间
+        const db = wx.cloud.database()
+        db.collection('USER').where({
+          UserId: this.data.openid
+        }).update({
+          data: {
+            ["TradeInfo.MemberTime"]: new Date().toLocaleString('chinese', {
+              hour12: false
+            })
+          },
+          success(res) {
+
+          },
+        })
+        console.log('推广积分')
+        db.collection("POINTS").add({
+          data: {
+            PointsType: "promoter",
+            RegistrantId: app.globalData.Guserid,
+            RegistrantPoints: 50,
+            ProductName: "会员手机认证",
+            // 直接推荐人
+            InviterId: app.globalData.Ginviterid,
+            InviterPoints: 30,
+            // 间接推荐人
+            IndirectInviterId: app.globalData.Gindirectinviterid,
+            IndirectInviterPoints: 10,
+            SysAddDate: new Date().getTime(),
+            AddDate: new Date().toLocaleString('chinese', {
+              hour12: false
+            }),
+            PointsStatus: "checked",
+          },
+          success(res) {
+            console.log("POINTS更新成功")
+            //给推荐和和管理员发送短信
+            if (app.globalData.Ginviterphone != undefined && app.globalData.Ginviterphone != "") {
+              var tempmobile = [18954744612, app.globalData.Ginviterphone]
+            } else {
+              var tempmobile = [18954744612]
+            }
+            // 调用云函数发短信给推荐人和管理员
+            wx.cloud.callFunction({
+              name: 'sendsms',
+              data: {
+                templateId: "1569087",
+                nocode: true,
+                mobile: tempmobile
+              },
+              success: res => {
+                console.log("短信发送结果", res)
+              },
+              fail: res => {
+                console.log(res)
+              },
+            })
+          },
+        })
+
+      }
+    } else {
+      wx.showToast({
+        title: '验证码错误',
+        icon: 'error',
+        duration: 2000
+      })
+
+    }
   },
   _pointsupdate() {
     this.data.temppoints = this.data.remainpoints - this.data.doneepoints
@@ -104,6 +282,11 @@ Page({
       transferpacketid: options.transferpacketid,
       params: options,
     })
+    if (app.globalData.Guserdata.UserInfo.UserPhone != "") {
+      this.setData({
+        loginshow: false
+      })
+    }
     // 调用方法初始化
     utils._setting()
     utils._productcheck()
