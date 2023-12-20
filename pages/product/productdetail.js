aaeaa2336247a9709ed02088e1675dd8cb6993f3
u011892213 <u011892213@gitcode.net>
2023-12-20 10:38:08 +0800

    20231220

--- pages/product/productdetail.js
+++ pages/product/productdetail.js
@@ -119,30 +119,28 @@ pagelink:"", //为管理员提供当前页面产品链接
       urls: imgList // 需要预览的图片http链接列表
     })
   },
-  //复制下载链接
-  bvCopyDownLink(e) {
-    var url = e.currentTarget.dataset.link; //获取data-link中的值
-    // var url=this.data.url;
-    wx.setClipboardData({
-      data: url,
-      success: function (res) {
-        wx.hideToast();
-        // self.setData({copyTip:true}),
-        wx.showModal({
-          title: '提示',
-          content: '该文件下载链接已复制到剪贴板，请打开手机浏览器，在手机浏览器地址栏中粘贴下载链接并下载、保存文件',
-          success: function (res) {
-            if (res.confirm) {
-              console.log('确定')
-            } else if (res.cancel) {
-              console.log('取消')
-            }
-          }
-        })
-
-      }
-    })
-  },
+  //   下载文件
+  bvDownLoad(e){
+    wx.showLoading({
+        title: '下载中',
+      });
+      let fileId=e.currentTarget.dataset.link
+      console.log("下载文件：",fileId);
+      wx.cloud.downloadFile({
+        fileID: fileId,
+        success: (result) => {
+          console.log("下载成功",result.tempFilePath);
+          utils._SuccessToast("下载成功")
+        },
+        fail: (res) => {
+          console.log("下载失败：",res);
+          utils._ErrorToast("下载失败")
+        },
+        complete: (res) => {
+          wx.hideLoading();
+        },
+      })
+},
   async bvReply(e) {
     
     console.log(e.currentTarget.dataset.id)
