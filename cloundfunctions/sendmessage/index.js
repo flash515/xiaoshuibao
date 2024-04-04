36ad9ac9067786ebbeb383150ad109db55bcada4
u011892213 <u011892213@gitcode.net>
2024-04-04 11:47:30 +0800

    20240404

---
+++ cloundfunctions/sendmessage/index.js
@@ -0,0 +1,49 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const QcloudSms = require("qcloudsms_js")
+const appid = "1400531781" // 替换成您申请的云短信 AppID 以及 AppKey
+const appkey = "acb976a3b5503d9ac47d84384f3dd90c"
+// const templateId = "985130" // 替换成您所申请模板 ID
+const smsSign = "小税宝" // 替换成您所申请的签名
+
+cloud.init({
+  env: 'xsbmain-9gvsp7vo651fd1a9',
+  traceUser: true,
+})
+// 生成6位数验证码 
+var code = "";
+for (var i = 0; i < 6; i++) {
+  var radom = Math.floor(Math.random() * 10);
+  code += radom;
+}
+// 云函数入口函数
+exports.main = async (event, context) => new Promise((resolve, reject) => {
+  /*单发短信示例为完整示例，更多功能请直接替换以下代码*/
+  if (event.nocode==true){
+    var params = []
+  }else{
+    var params = [code]
+  }
+  var qcloudsms = QcloudSms(appid, appkey);
+  var ssender = qcloudsms.SmsSingleSender();
+  // 获取发送短信的手机号码
+  var mobile = event.mobile
+  var templateId = event.templateId
+  // 获取手机号国家/地区码
+  var nationcode = event.nationcode
+  ssender.sendWithParam(nationcode, mobile, templateId, params, smsSign, "", "", (err, res, resData) => {
+    /*设置请求回调处理, 这里只是演示，您需要自定义相应处理逻辑*/
+    if (err) {
+      console.log("err: ", err);
+      reject({
+        err
+      })
+    } else {
+      resolve({
+        res: res.req,
+        resData
+      })
+    }
+  });
+
+})
--- cloundfunctions/sendsms/index.js
+++ cloundfunctions/sendsms/index.js
@@ -2,57 +2,44 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 const QcloudSms = require("qcloudsms_js")
+// 以下为小税宝AppID信息，有效，使用创企服信息无效
 const appid = "1400531781" // 替换成您申请的云短信 AppID 以及 AppKey
 const appkey = "acb976a3b5503d9ac47d84384f3dd90c"
-// const templateId = "" // 替换成您所申请模板 ID
-const smsSign = "小税宝" // 替换成您所申请的签名
 
 cloud.init({
-  env: 'xsbmain-9gvsp7vo651fd1a9',
-  traceUser: true,
+    env: 'xsbmain-9gvsp7vo651fd1a9',
+    traceUser: true,
 })
- // 生成6位数验证码 
+// 生成4位数验证码 
 var code = "";
-for (var i = 0; i < 6; i++) {
+for (var i = 0; i < 4; i++) {
     var radom = Math.floor(Math.random() * 10);
     code += radom;
- }
+}
 // 云函数入口函数
-exports.main = async (event, context) => new Promise((resolve, reject) => {    
- /*单发短信示例为完整示例，更多功能请直接替换以下代码*/
- if (event.nocode==true){
-  var params = []
-}else{
-  var params = [code]
-}
- var qcloudsms = QcloudSms(appid, appkey);
-//  带模版单条发送的接口
- var single = qcloudsms.SmsSingleSender();
-//  带模版群发的接口
- var multi = qcloudsms.SmsMultiSender();
-var templateId = event.templateId
- // 获取发送短信的手机号码
- var mobile = event.mobile
- // 获取手机号国家/地区码，国内发送短信可以直接86写在接口里
-//  var nationcode = event.nationcode
-//  single.sendWithParam(nationcode, mobile, templateId, params, smsSign, "", "", (err, res, resData) => {
-//      /*设置请求回调处理, 这里只是演示，您需要自定义相应处理逻辑*/
-//      if (err) {
-//        console.log("err: ", err);
-//        reject({ err })
-//      } else {
-//        resolve({ res: res.req, resData })
-//      }
-//    }
-//  );
- multi.sendWithParam(86, mobile, templateId, params, smsSign, "", "", (err, res, resData) => {
-  /*设置请求回调处理, 这里只是演示，您需要自定义相应处理逻辑*/
-  if (err) {
-    console.log("err: ", err);
-    reject({ err })
-  } else {
-    resolve({ res: res.req, resData })
-  }
-}
-);
+exports.main = async (event, context) => new Promise((resolve, reject) => {
+    /*单发短信示例为完整示例，更多功能请直接替换以下代码*/
+    if (event.nocode == true) {
+        var params = []
+    } else {
+        var params = [code]
+    }
+    var qcloudsms = QcloudSms(appid, appkey);
+    //  带模版群发的接口
+    var multi = qcloudsms.SmsMultiSender();
+    var templateId = event.templateId
+    // 获取发送短信的手机号码及签名
+    var mobile = event.mobile
+    var smsSign = event.Sign
+
+     multi.sendWithParam(86, mobile, templateId, params, smsSign, "", "", (err, res, resData) => {
+      /*设置请求回调处理, 这里只是演示，您需要自定义相应处理逻辑*/
+      if (err) {
+        console.log("err: ", err);
+        reject({ err })
+      } else {
+        resolve({ res: res.req, resData })
+      }
+    }
+    );
 })
